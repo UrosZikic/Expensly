6d3004f3f695e9aefee3c4ae92a4f94c74d4e2b1
UrosZikic <uroszikic1512988@gmail.com>
2025-05-19 00:53:33 +0200

    registration validate update

--- pages/sign_up.php
+++ pages/sign_up.php
@@ -1,30 +1,59 @@
 <?php
 require_once '../components/head.php';
 session_start();
-
-if (isset($_SESSION['m_s_d']))
-  print_r($_SESSION['m_s_d']);
+$validate = true;
+if (isset($_SESSION['m_s_d'])) {
+  $validate_prev_try = $_SESSION["m_s_d"];
+  if ($validate_prev_try['error'] != "")
+    // $validate will trigger outfill on input fields only in case of an actual error
+    print_r($_SESSION['m_s_d']);
+}
 ?>
-<div>
 
+<div class="register_notification_container center_object">
+  <h1>WARNING</h1>
+  <?php
+  switch ($validate_prev_try['error']) {
+    case 'email_invalid':
+      echo 'Invalid Email Address';
+      break;
+    case 'email_duplicate':
+      echo 'Email Address Already In Use';
+      break;
+    case 'nickname_duplicate':
+      echo 'Username Already In Use';
+      break;
+    case 'password_!_match':
+      echo "Passwords Don't Match";
+      break;
+    default:
+      echo "Successful Registration!";
+      break;
+  }
+
+  ?>
 </div>
+
+
 <div class="registration_container d_flex flex_gap_s">
   <form class="d_flex flex_dir_col flex_gap_xs form_width" action="su_validate.php" method="post">
     <h2>Register your profile!</h2>
-    <input type="text" name="full_name" placeholder="enter your name" required>
-    <input type="text" name="nickname" placeholder="enter your nickname" required>
-    <input type="email" name="email" placeholder="enter your email" required>
-    <input type="password" name="password" placeholder="enter your password" required>
-    <input type="password" name="re_pass" placeholder="repeat your password" required>
-    <input type="submit">
-  </form>
-  <form class="hidden" action="" method="post">
-    <input type="email" name="email" placeholder="enter your email">
-    <input type="password" name="password" placeholder="enter your password" required>
-    <input type="submit">
-  </form>
-</div>
+    <input type="text" name="full_name" placeholder="enter your name" value="<?php if ($validate)
+      echo $validate_prev_try['full_name'] ?>" required>
+      <input type="text" name="nickname" placeholder="enter your nickname" value="<?php if ($validate && !isset($_GET['nickname']))
+      echo $validate_prev_try['nick_name'] ?>" required>
+      <input type="email" name="email" placeholder="enter your email" value="<?php if ($validate && !isset($_GET['email']))
+      echo $validate_prev_try['email'];
+    else
+      echo "" ?>" required>
+      <input type="password" name="password" placeholder="enter your password" value="<?php if ($validate && !isset($_GET['password']))
+      echo $validate_prev_try['password'] ?>" required>
+      <input type="password" name="re_pass" placeholder="repeat your password" value="<?php if ($validate && !isset($_GET['password']))
+      echo $validate_prev_try['password'] ?>" required>
+      <input type="submit">
+    </form>
+  </div>
 
-<?php
-require_once '../components/foot.php';
-?>
+  <?php
+    require_once '../components/foot.php';
+    ?>
--- pages/su_validate.php
+++ pages/su_validate.php
@@ -10,17 +10,53 @@ $pass_hash = password_hash($password, PASSWORD_DEFAULT);
 $re_pass = $_POST['re_pass'];
 
 
-$memorize_signup_details = ['name' => $full_name, 'nick_name' => $nickname, "email" => $email, "password" => $password];
+$memorize_signup_details = ['full_name' => $full_name, 'nick_name' => $nickname, "email" => $email, "password" => $password, 'error' => ""];
 $_SESSION['m_s_d'] = $memorize_signup_details;
 
 if (!$email) {
+  $memorize_signup_details['error'] = 'email_invalid';
+  $memorize_signup_details['email'] = '';
+  $_SESSION['m_s_d'] = $memorize_signup_details;
   Header("Location: sign_up.php");
   exit;
 }
 
-if ($password !== $re_pass) {
+// Validate email
+$stmt_e = $conn->prepare("SELECT email FROM profiles WHERE email = :email");
+$stmt_e->bindParam(':email', $email);
+$stmt_e->execute();
+$emails = $stmt_e->fetchAll(PDO::FETCH_ASSOC);
+if ($emails) {
+  $memorize_signup_details['error'] = 'email_duplicate';
+  $memorize_signup_details['email'] = "";
+  $_SESSION['m_s_d'] = $memorize_signup_details;
 
-  Header("Location: sign_up.php?error=10010");
+  Header("Location: sign_up.php?email=duplicate");
+  exit;
+}
+// Validate nickname
+$stmt_nn = $conn->prepare("SELECT nickname FROM profiles WHERE nickname = :nickname");
+$stmt_nn->bindParam(':nickname', $nickname);
+$stmt_nn->execute();
+$nicknames = $stmt_nn->fetchAll(PDO::FETCH_ASSOC);
+if ($nicknames) {
+  $memorize_signup_details['error'] = 'nickname_duplicate';
+  $memorize_signup_details['nick_name'] = '';
+
+  $_SESSION['m_s_d'] = $memorize_signup_details;
+
+  Header("Location: sign_up.php?nickname=duplicate");
+  exit;
+}
+
+// 
+
+
+if ($password !== $re_pass) {
+  $memorize_signup_details['error'] = 'password_!_match';
+  $memorize_signup_details['password'] = '';
+  $_SESSION['m_s_d'] = $memorize_signup_details;
+  Header("Location: sign_up.php?error=password");
   exit;
 }
 
@@ -31,11 +67,12 @@ $stmt = $conn->prepare("INSERT INTO profiles (full_name, nickname, email, pass)
 $stmt->bindParam(':full_name', $full_name);
 $stmt->bindParam(':nickname', $nickname);
 $stmt->bindParam(':email', $email);
-$stmt->bindParam(':pass', $password);
+$stmt->bindParam(':pass', $pass_hash);
 
 try {
   $stmt->execute();
   echo "<p>Success</p>";
+  header('Location: sign_in.php?success=login');
 } catch (PDOException $e) {
   if ($e->getCode() == 23000) {
     // Duplicate entry error
